5665419d0f5a1e597c09f011cea571976ee3eb5f
tanyap <biddanda.tanya@gmail.com>
2020-04-23 14:10:06 -0700

    new pseudo code, rethinking process for more streamlined solution

---
+++ script.js
@@ -0,0 +1,20 @@
+var pwdLength = prompt("Enter a number between 8 and 128.")
+var isUpper = true
+var isLower = true
+var isSpecial = true
+
+
+//prompt("Would you like your password to have uppercase letters?") t/f
+//prompt("Would you like your password to have lowercase letters?") t/f
+//prompt("Would you like your password to have special characters?") t/f
+
+//Error: must choose at least one character type
+
+console.log(isUpper.toUpperCase())
+
+// create a random number (character?) generator, loop it until x (x being the password length) is attained
+// if Uppercase, convert random characters from array to Uppercase
+// if Lowercase, convert random characters from array to Lowercase
+// if isSpecial, convert random characters from array to isSpecial.
+
+//OR add character types to single array, choose final password from that array**
